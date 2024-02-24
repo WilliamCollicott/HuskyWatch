40b0e08f114318872a20d10dac63b7db429caa56
William Collicott <31823020+WilliamCollicott@users.noreply.github.com>
2024-02-24 13:30:36 -0600

    Use selenium except for RSS, removed future/former player sorting from setup() function.

--- husky_watch.py
+++ husky_watch.py
@@ -10,12 +10,13 @@
 # Email  : william@collicott.com
 
 import datetime
-import feedparser
 import re
-import requests
+import time
+import feedparser
 from webhook_url import webhook_url
 from discord_webhook import DiscordWebhook
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 # This method parses a transfer's description section and assembles the string representing the message to be published 
 def construct_message(title, decoded_description, type):
@@ -45,7 +46,7 @@ def process_match(transaction_id, transaction_ids_list, title, decoded_descripti
     message = construct_message(title, decoded_description, type)
 
     # Record the transaction's ID so we know not to publish it again it we still see it later on
-    with open('/volume3/WilliamCloud/HuskyWatch/transaction_ids.txt', 'a') as transaction_ids_file:
+    with open('transaction_ids.txt', 'a') as transaction_ids_file:
         date_and_time = datetime.datetime.now()
         transaction_ids_file.write(transaction_id + ',' + str(date_and_time) + '\n')
 
@@ -55,14 +56,12 @@ def process_match(transaction_id, transaction_ids_list, title, decoded_descripti
 
 # Assemble the lists of players to look out for and the list of transactions that have already been published
 def setup():
-    future_players_of_interest = []     # list of player IDs of future MTU players whose transfers we want to flag
-    former_players_of_interest = []     # list of player IDs of former MTU players whose transfers we want to flag
     transaction_ids_list = []           # list of transaction IDs that we've published less than 14 days ago
     transaction_lines_to_add_back = []  # transactions in transaction_ids.txt that were published less than 14 days ago
     script_invocation_time = datetime.datetime.now()
 
     # Loop through each transaction listed in transaction_ids.txt to determine if we still need to keep track of it
-    with open('/volume3/WilliamCloud/HuskyWatch/transaction_ids.txt', 'r') as transaction_ids_file:
+    with open('transaction_ids.txt', 'r') as transaction_ids_file:
         transaction_ids_file_lines = transaction_ids_file.readlines()
         
         for line in transaction_ids_file_lines:
@@ -81,67 +80,66 @@ def setup():
             transaction_lines_to_add_back.append(line)
             
     # Clear the transaction_ids.txt file and only write back the lines whose transactions we still want to keep track of
-    with open('/volume3/WilliamCloud/HuskyWatch/transaction_ids.txt', 'w') as transaction_ids_file:
+    with open('transaction_ids.txt', 'w') as transaction_ids_file:
         for line in transaction_lines_to_add_back:
             transaction_ids_file.write(line)
 
     # Gather IDs for players of interest (will play for Michigan Tech in the future, or played for Michigan Tech before)
-    where_are_they_now_url = 'https://www.eliteprospects.com/team/548/michigan-tech/where-are-they-now?sort=tp'
-    poi_data = requests.get(where_are_they_now_url)
-    poi_html = BeautifulSoup(poi_data.text, 'html.parser')
+    where_are_they_now_driver = webdriver.Chrome()
+    where_are_they_now_driver.get('https://www.eliteprospects.com/team/548/michigan-tech/where-are-they-now?sort=tp')
+    time.sleep(5)
+    poi_html = BeautifulSoup(where_are_they_now_driver.page_source, features='html.parser')
     poi_player_tables = poi_html.select('div.expandable-table-wrapper')
 
-    # Create a list of all future and former player IDs on Michigan Tech's "Where Are They Now?" page
-    poi_player_pages_and_ids = re.findall(r'<a href=\"(https://www\.eliteprospects\.com/player/(\d*)/.*)\">.*</a>', str(poi_player_tables))
-    
-    # Separate the poi_player_links_and_ids list in to one for future players, and another for former ones
-    for entry in poi_player_pages_and_ids:
-        poi_page_data = requests.get(entry[0] + '?league=NCAA')
-        poi_page_html = BeautifulSoup(poi_page_data.text, 'html.parser')
-        stats_section = poi_page_html.find('div', {'id': 'league-stats'})
-        ncaa_section = stats_section.find_all('tr', {'data-league': 'NCAA'})
+    # Create a list of player page URLs for all future and former players on Michigan Tech's "Where Are They Now?" page
+    poi_player_page_urls = re.findall(r'<a href=\"(https://www\.eliteprospects\.com/player/\d*/.*)\">.*</a>', str(poi_player_tables))
 
-        if len(ncaa_section) == 1 and ncaa_section[0].find('td', {'class': 'regular gp'}).text == '-*':
-            # The player is a future MTU player
-            future_players_of_interest.append(entry[1])
-        else:
-            # The player is a former MTU player
-            former_players_of_interest.append(entry[1])
+    print(poi_player_page_urls)
 
-    return future_players_of_interest, former_players_of_interest, transaction_ids_list
+    return poi_player_page_urls, transaction_ids_list
 
-def process_feed(feed, future_players_of_interest, former_players_of_interest, transaction_ids_list):
+def process_feed(feed, poi_player_page_urls, transaction_ids_list):
+    if len(feed) == 0:
+        raise Exception("The list of RSS feed entries is 0")
+
     # In each the RSS feed's 50 most recent transfers, look for mentions of Michigan Tech or future or former players
     for item in feed.entries:
         transaction_id = re.search(r'/t/(\d*)', item.guid).group(1)
         decoded_description = str(BeautifulSoup(item.description, features='html.parser'))
+        match_type = ''
 
         if re.search(r'From: <a href="https:\/\/www.eliteprospects.com\/team\/548\/', decoded_description):
             # A player is leaving Michigan Tech
-            process_match(transaction_id, transaction_ids_list, item.title, decoded_description, 'Departure')
-
+            match_type = 'Departure'
         elif re.search(r'To: <a href="https:\/\/www.eliteprospects.com\/team\/548\/', decoded_description):
             # A player is joining Michigan Tech
-            process_match(transaction_id, transaction_ids_list, item.title, decoded_description, 'Arrival')
-
+            match_type = 'Arrival'
         else:
             # If Michigan Tech is not mentioned in the transfer, check to see if a future or former player is involved
-            for player_id in future_players_of_interest:
-                player_id_regex = '/player/' + player_id
-                if re.search(player_id_regex, decoded_description):
-                    # A future Michigan Tech player is involved in this transfer in some way
-                    process_match(transaction_id, transaction_ids_list, item.title, decoded_description, 'Future Player')
+            for url in poi_player_page_urls:
+                if re.search(url, decoded_description):
+                    # A future or former Michigan Tech player is involved in this transaction
+                    poi_driver = webdriver.Chrome()
+                    poi_driver.get(url + '?league=NCAA')
+                    time.sleep(5)
+                    poi_page_html = BeautifulSoup(poi_driver.page_source, features='html.parser')
+                    stats_section = poi_page_html.find('div', {'id': 'league-stats'})
+                    ncaa_section = stats_section.find_all('tr', {'data-league': 'NCAA'})
 
-            for player_id in former_players_of_interest:
-                player_id_regex = '/player/' + player_id
-                if re.search(player_id_regex, decoded_description):
-                    # A former Michigan Tech player is involved in this transfer in some way
-                    process_match(transaction_id, transaction_ids_list, item.title, decoded_description, 'Former Player')
+                    if len(ncaa_section) == 1 and ncaa_section[0].find('td', {'class': 'regular gp'}).text == '-*':
+                        # The player is a future MTU player
+                        match_type = 'Future Player'
+                    else:
+                        # The player is a former MTU player
+                        match_type = 'Former Player'
+
+        if match_type != '':
+            process_match(transaction_id, transaction_ids_list, item.title, decoded_description, match_type)
 
 def main():
+    poi_player_page_urls, transaction_ids_list = setup()
     feed = feedparser.parse('https://www.eliteprospects.com/rss/transfers')
-    future_players_of_interest, former_players_of_interest, transaction_ids_list = setup()
-    process_feed(feed, future_players_of_interest, former_players_of_interest, transaction_ids_list)
+    process_feed(feed, poi_player_page_urls, transaction_ids_list)
 
 if __name__ == "__main__": 
     main()
